a9b0f35883224d4cc7b69b5b487ef5453d5f23c0
Bang Alex <masfatahdiamond@gmail.com>
2019-12-01 22:16:12 +0700

    Membuat Conttoller Register With Ajax Engine

---
+++ application/controllers/Register.php
@@ -0,0 +1,44 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Register extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('m_login');
+	}
+
+	public function index(){
+		$user = trim($this->input->post('Ruser'));
+		$pass = md5(trim($this->input->post('Rpass')));
+		$nick = trim($this->input->post('Rnick'));
+		$mail = trim($this->input->post('Rmail'));
+		$cekuser = $this->m_login->cek("username",$user);
+		$ceknick = $this->m_login->cek("nickname",$nick);
+		$cekmail = $this->m_login->cek("email",$mail);
+		if($cekuser->num_rows() > 0){
+			echo "ada";
+		}elseif($ceknick->num_rows() > 0){
+			echo "ada";
+		}elseif($cekmail->num_rows() > 0){
+			echo "ada";
+		}elseif(empty($user) || empty($pass) || empty($nick) || empty($mail)){
+			echo "no";
+		}else{
+			$data = array(
+				'username' => $user,
+				'password' => $pass,
+				'nickname' => $nick,
+				'email' => $mail
+			);
+			$save = $this->m_login->insert($data);
+			if($save){
+				$this->session->set_userdata('nickname',$nick);
+				$this->m_login->set_online($this->session->userdata('nickname'),date("h:i:s"));
+				echo "yes";
+			}else{
+				echo "no";
+			}	
+		}
+	}
+}
